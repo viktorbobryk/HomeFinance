e4b5e8fec89b55a6b59257893183e0ddfe21bbd3
Viktor Sydorov <victor.sydorov@gmail.com>
2019-02-06 22:21:05 +0200

    firebase database started

--- src/containers/AboutUs/AboutUs.js
+++ src/containers/AboutUs/AboutUs.js
@@ -1,14 +1,29 @@
 import React, {Component} from 'react';
-
+import {databaseRef} from "../../config/firebase";
 class AboutAs extends Component {
 
   state = {
 
   };
 
+   createUser=function(){
+       console.log(databaseRef);
+      var a= databaseRef.ref("users");
+              a.push().set({
+    username: "asd",
+    email: "email@mail.com"
+    
+  });
+       
+   };
+    
+
   render() {
     return (
+            <div>
       <h2>About Us</h2>
+      <button onClick={this.createUser}>Create user</button>
+      </div>
     );
   }
 }
